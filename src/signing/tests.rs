e80d1565c287a9fb97b3fa9e02a2a70ecc558e2f
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-30 01:11:36 +0300

    wip

--- src/signing/tests.rs
+++ src/signing/tests.rs
@@ -10,53 +10,8 @@ use crate::signing::{setup_signature, sign, VerificationParameters, verify};
 fn signing() {
     let data = "c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0";
 
-
-    let dsa_ref = Dsa::generate(4096).unwrap();
-    let p = dsa_ref.p();
-    let q = dsa_ref.q();
-    let g = dsa_ref.g();
-
-    let public_key = dsa_ref.pub_key();
-    let private_key = dsa_ref.priv_key();
-
-    println!("p: {}", p);
-    println!("q: {}", q);
-    println!("g: {}", g);
-    println!("private_key: {}", private_key);
-    println!("public_key: {}", public_key);
-
-    let private_key = Dsa::from_private_components(
-        BigNumRef::to_owned(p).unwrap(),
-        BigNumRef::to_owned(q).unwrap(),
-        BigNumRef::to_owned(g).unwrap(),
-        BigNumRef::to_owned(private_key).unwrap(),
-        BigNumRef::to_owned(public_key).unwrap(),
-    ).unwrap();
-
-
-    let private_key = PKey::from_dsa(private_key).unwrap();
-
-    let public_key = Dsa::from_public_components(
-        BigNumRef::to_owned(p).unwrap(),
-        BigNumRef::to_owned(q).unwrap(),
-        BigNumRef::to_owned(g).unwrap(),
-        BigNumRef::to_owned(public_key).unwrap(),
-    ).unwrap();
-
-    let public_key = PKey::from_dsa(public_key).unwrap();
-
-
-    let mut signer = Signer::new(MessageDigest::sha256(), &private_key).unwrap();
-    signer.update(data.as_bytes()).unwrap();
-
-    let signature = signer.sign_to_vec().unwrap();
-    let mut verifier = Verifier::new(MessageDigest::sha256(), &public_key).unwrap();
-    verifier.update(data.as_bytes()).unwrap();
-
-    assert!(verifier.verify(signature.as_ref()).unwrap());
-
-    let params = setup_signature(Some("/test/signature_parameters/")).unwrap();
-    let signature = sign(params, data.as_bytes());
+    let signature_params = setup_signature(Some("/test/signature_parameters/")).unwrap();
+    let signature = sign(signature_params, data.as_bytes());
 
     let params = setup_signature(Some("/test/signature_parameters/")).unwrap();
     let verification_params = VerificationParameters {
