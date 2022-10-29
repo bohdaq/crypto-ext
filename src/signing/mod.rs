9ce465b8c0960a755fbb052d8e2ad20eff4bf251
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-29 19:19:41 +0300

    wip

--- src/signing/mod.rs
+++ src/signing/mod.rs
@@ -1,5 +1,5 @@
 use openssl::dsa::Dsa;
-use crate::encryption::{get_path_relative_to_working_directory, get_static_filepath};
+use crate::encryption::{generate_passphrase, get_path_relative_to_working_directory, get_static_filepath, read_or_create_and_write};
 
 #[cfg(test)]
 mod tests;
@@ -28,7 +28,33 @@ pub fn setup_signature(path_to_encryption_parameters: Option<&str>) -> Result<Si
     if boxed_dsa_p_path.is_err() {
         return Err(boxed_dsa_p_path.err().unwrap());
     }
+
     let dsa_p_path = boxed_dsa_p_path.unwrap();
+    let boxed_p = get_or_create_value_at_path(dsa_p_path.as_str(), p.to_string().as_str());
+    if boxed_p.is_err() {
+        return Err(boxed_p.err().unwrap());
+    }
+
+    let signature_parameters = SignatureParameters{
+        dsa_p: boxed_p.unwrap(),
+        dsa_q: "".to_string(),
+        dsa_g: "".to_string(),
+        dsa_private_key: "".to_string(),
+        dsa_public_key: "".to_string()
+    };
+    Ok(signature_parameters)
+}
+
+pub fn get_or_create_value_at_path(path: &str, value: &str) -> Result<String, String> {
+
+    let boxed_passphrase = read_or_create_and_write(path, value);
+    if boxed_passphrase.is_err() {
+        let message = boxed_passphrase.err().unwrap();
+        return Err(message)
+    }
+
+    let passphrase = boxed_passphrase.unwrap();
+    Ok(passphrase)
 }
 
 pub fn sign(private_key: &str, passphrase: &str, data: &[u8]) -> String {
