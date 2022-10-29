72436758d567205d963477e02d2ddd54d29a3902
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-29 14:22:38 +0300

    wip

--- src/signing/mod.rs
+++ src/signing/mod.rs
@@ -4,7 +4,9 @@ mod tests;
 
 pub const DSA_SIZE: u32 = 4096;
 
+pub fn setup_signature(path_to_encryption_parameters: Option<&str>) {
 
+}
 
 pub fn sign(private_key: &str, passphrase: &str, data: &[u8]) -> String {
     //TODO:
