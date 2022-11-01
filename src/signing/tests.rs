74a33d3c1372d8d6cfeb172cba0e40cc07cd462f
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-01 19:06:12 +0200

    wip

--- src/signing/tests.rs
+++ src/signing/tests.rs
@@ -16,3 +16,17 @@ fn signing() {
 
     assert!(is_verified);
 }
+
+#[test]
+fn signing_alternative() {
+    let data = "c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0";
+    let path_to_params = "/test/signature_parameters/";
+
+    let (signature_params, verification_params) = setup(Some(path_to_params)).unwrap();
+
+    let signature = sign(signature_params, data.as_bytes()).unwrap();
+
+    let is_verified = verify(verification_params, data.as_bytes(), signature).unwrap();
+
+    assert!(is_verified);
+}
