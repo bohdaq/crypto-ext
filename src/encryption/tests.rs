3c332e074b0496b69984c792790a0e1ef984cc59
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-01 19:21:07 +0200

    wip

--- src/encryption/tests.rs
+++ src/encryption/tests.rs
@@ -2,6 +2,10 @@ use crate::encryption::{decrypt, encrypt, get_decryption_params, get_encryption_
 
 #[test]
 fn encryption() {
+    //maximum 501 bytes at once to be encrypted
+    let data = "Some random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomee123textSomee123textSomee123textSo";
+    println!("data len: {}", data.as_bytes().len());
+    
     // path needs to be accessible by user with write permission for initial setup
     let params_path = "/test/encryption_parameters/";
     // it will read encryption params like public, private keys and passphrase or create them
@@ -9,10 +13,6 @@ fn encryption() {
     let _ = setup(Some(params_path));
 
     let params  = get_encryption_params(Some(params_path)).unwrap();
-
-    //maximum 501 bytes at once to be encrypted
-    let data = "Some random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomeeSome random textSome random textSome random textSome random textSome random textSome random textSome random textSomee123textSomee123textSomee123textSo";
-    println!("data len: {}", data.as_bytes().len());
     let encrypted_u8 = encrypt(params, data.as_bytes()).unwrap();
 
     let params = get_decryption_params(Some(params_path)).unwrap();
@@ -32,7 +32,7 @@ fn encryption_alternative() {
 
     // path needs to be accessible by user with write permission for initial setup
     let params_path = "/test/encryption_parameters/";
-    // it will read encryption params like public, private keys and passphrase or create them
+    // this will create encryption params like public, private keys and passphrase
     let (encryption_params, decryption_params) = setup(Some(params_path)).unwrap();
 
 
