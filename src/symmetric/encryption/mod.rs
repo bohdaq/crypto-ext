33c0dfb562100dfa6d9f77ef3efea06365b899a4
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-05 00:46:46 +0200

    wip

--- src/symmetric/encryption/mod.rs
+++ src/symmetric/encryption/mod.rs
@@ -1,3 +1,6 @@
+use aes_gcm::aead::{generic_array::GenericArray, Aead, KeyInit, Payload};
+use aes_gcm::Aes128Gcm;
+
 #[cfg(test)]
 mod tests;
 
@@ -11,9 +14,23 @@ pub struct DecryptionParameters {
     pub nonce: String,
 }
 
-pub fn encrypt(params: EncryptionParameters, data_to_encrypt: &[u8], associated_data: &[u8]) -> Vec<u8> {
-    //TODO:
-    let encrypted_data = vec![];
+pub fn encrypt(params: EncryptionParameters, data_to_encrypt: &[u8], associated_data: &[u8]) -> Result<Vec<u8>, String> {
+    let payload = Payload {
+        msg: data_to_encrypt,
+        aad: associated_data,
+    };
 
-    encrypted_data
+    let key = GenericArray::from_slice(params.key.as_bytes());
+    let nonce = GenericArray::from_slice(params.nonce.as_bytes());
+
+    let cipher = Aes128Gcm::new(key);
+    let boxed_cipher_text = cipher.encrypt(nonce, payload);
+    if boxed_cipher_text.is_err() {
+        let message = boxed_cipher_text.err().unwrap().to_string();
+        return Err(message)
+    }
+
+    let cipher_text = boxed_cipher_text.unwrap();
+
+    Ok(cipher_text)
 }
--- src/symmetric/encryption/tests.rs
+++ src/symmetric/encryption/tests.rs
@@ -1,35 +1,40 @@
 use crate::generate_passphrase;
 use aes_gcm::aead::{generic_array::GenericArray, Aead, KeyInit, Payload};
 use aes_gcm::Aes128Gcm;
+use crate::symmetric::encryption::{encrypt, EncryptionParameters};
 
 #[test]
 fn encryption() {
-    let data = "some test text".to_string();
+    let data = "some data to encrypt";
+    let associated_data = "some unencrypted data that needs to be sent along the encrypted data and won't be changed during transmission by a hacker";
 
-    let key = generate_passphrase().unwrap();
+    let passphrase_64_bytes = generate_passphrase().unwrap();
 
-    let nonce = generate_passphrase().unwrap();
+    // key is 16 bytes long
+    let key = passphrase_64_bytes[48..64].to_string();
 
-    let aad = "";
+    // nonce is 12 bytes long
+    let nonce = passphrase_64_bytes[36..48].to_string();
 
-    let payload = Payload {
-        msg: data.as_bytes(),
-        aad: aad.as_bytes(),
+    let params = EncryptionParameters {
+        key,
+        nonce,
     };
+    let cipher_text = encrypt(params, data.as_bytes(), associated_data.as_bytes()).unwrap();
 
-    let key = GenericArray::from_slice(key[48..64].as_bytes());
-    let nonce = GenericArray::from_slice(nonce[36..48].as_bytes());
-
-    let cipher = Aes128Gcm::new(key);
-
-    let cipher_text = cipher.encrypt(nonce, payload).unwrap();
-    let (_ct, _tag) = cipher_text.split_at(cipher_text.len() - 16);
+    // decryption
 
     let payload = Payload {
         msg: &cipher_text,
-        aad: aad.as_bytes(),
+        aad: associated_data.as_bytes(),
     };
 
+    let key = passphrase_64_bytes[48..64].to_string();
+    let nonce = passphrase_64_bytes[36..48].to_string();
+
+    let key = GenericArray::from_slice(key.as_bytes());
+    let nonce = GenericArray::from_slice(nonce.as_bytes());
+    let cipher = Aes128Gcm::new(key);
     let plain_text = cipher.decrypt(nonce, payload).unwrap();
 
     assert_eq!(data.as_bytes(), plain_text);
