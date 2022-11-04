4689116f865dff1e90387c782457a599cc0d35a4
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-04 22:17:30 +0200

    wip

--- src/symmetric/encryption/mod.rs
+++ src/symmetric/encryption/mod.rs
@@ -10,3 +10,10 @@ pub struct DecryptionParameters {
     pub key: String,
     pub nonce: String,
 }
+
+pub fn encrypt(params: EncryptionParameters, data_to_encrypt: &[u8], associated_data: &[u8]) -> Vec<u8> {
+    //TODO:
+    let encrypted_data = vec![];
+
+    encrypted_data
+}
