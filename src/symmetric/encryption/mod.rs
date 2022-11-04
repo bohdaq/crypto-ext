677c79ed456999d522b50ee57bd6c6b8cba04cc8
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-04 18:11:22 +0200

    wip

--- src/symmetric/encryption/mod.rs
+++ src/symmetric/encryption/mod.rs
@@ -1,2 +1,12 @@
 #[cfg(test)]
 mod tests;
+
+pub struct EncryptionParameters {
+    pub key: String,
+    pub nonce: String,
+}
+
+pub struct DecryptionParameters {
+    pub key: String,
+    pub nonce: String,
+}
