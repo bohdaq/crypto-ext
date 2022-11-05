02c51a9f2997a9e947f76a0a9e6e057aab27cefb
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-05 16:22:33 +0200

    wip

--- src/symmetric/encryption/tests.rs
+++ src/symmetric/encryption/tests.rs
@@ -14,19 +14,16 @@ fn encryption() {
     // nonce is 12 bytes long
     let nonce = passphrase_64_bytes[36..48].to_string();
 
-    let params = EncryptionParameters {
-        key,
-        nonce,
-    };
-    let cipher_text = encrypt(params, data.as_bytes(), associated_data.as_bytes()).unwrap();
+    let params = EncryptionParameters { key, nonce };
+    let encrypted_data = encrypt(params, data.as_bytes(), associated_data.as_bytes()).unwrap();
+
 
     // decryption
-
     let key = passphrase_64_bytes[48..64].to_string();
     let nonce = passphrase_64_bytes[36..48].to_string();
 
     let params = DecryptionParameters{ key, nonce };
-    let plain_text = decrypt(params, cipher_text.as_slice(), associated_data.as_ref()).unwrap();
+    let plain_text = decrypt(params, encrypted_data.as_slice(), associated_data.as_ref()).unwrap();
 
     assert_eq!(data.as_bytes(), plain_text);
 
