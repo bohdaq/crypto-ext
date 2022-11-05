dd70dd75ea851aa216da9271a02b7a14542760ac
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-05 16:40:11 +0200

    wip

--- src/asymmetric/signing/tests.rs
+++ src/asymmetric/signing/tests.rs
@@ -3,7 +3,7 @@ use crate::asymmetric::signing::{get_signature_params, get_verification_params, 
 
 #[test]
 fn signing() {
-    let data = "c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0";
+    let data = "some data to sign".as_bytes();
     let path_to_params = "/test/signature_parameters/";
 
     // setup will read, or populate required parameters for signing and verification
@@ -11,24 +11,24 @@ fn signing() {
     let _ = setup(Some(path_to_params)).unwrap();
 
     let signature_params = get_signature_params(Some(path_to_params)).unwrap();
-    let signature = sign(signature_params, data.as_bytes()).unwrap();
+    let signature = sign(signature_params, data).unwrap();
 
     let verification_params = get_verification_params(Some(path_to_params)).unwrap();
-    let is_verified = verify(verification_params, data.as_bytes(), signature).unwrap();
+    let is_verified = verify(verification_params, data, signature).unwrap();
 
     assert!(is_verified);
 }
 
 #[test]
 fn signing_alternative() {
-    let data = "c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0c29tZSB0ZXh0";
+    let data = "some data to sign".as_bytes();
     let path_to_params = "/test/signature_parameters/";
 
     let (signature_params, verification_params) = setup(Some(path_to_params)).unwrap();
 
-    let signature = sign(signature_params, data.as_bytes()).unwrap();
+    let signature = sign(signature_params, data).unwrap();
 
-    let is_verified = verify(verification_params, data.as_bytes(), signature).unwrap();
+    let is_verified = verify(verification_params, data, signature).unwrap();
 
     assert!(is_verified);
 }
