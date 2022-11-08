439f354efbc66f3699d5feaadf6fe2376cd96acf
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-08 03:18:54 +0200

    wip

--- src/asymmetric/encryption/mod.rs
+++ src/asymmetric/encryption/mod.rs
@@ -131,7 +131,33 @@ pub fn get_decryption_params(path_to_encryption_parameters: Option<&str>) -> Res
     Ok(decryption_params)
 }
 
-
+/// Encrypts given byte array of maximum length up to 501 bytes
+///
+/// # Examples
+///
+/// ```
+///    use crypto_ext::asymmetric::encryption::{encrypt, decrypt, EncryptionParameters, DecryptionParameters, setup, get_encryption_params, get_decryption_params};
+///
+///    #[test]
+///    fn encryption() {
+///        //maximum 501 bytes at once to be encrypted
+///        let data_to_encrypt_as_bytes = "Some data to encrypt".as_bytes();
+///
+///        // path needs to be accessible by user with write permission for initial setup
+///        let params_path = "/test/encryption_parameters/";
+///        // it will read encryption params like public, private keys and passphrase or create them
+///        // in this example setup is used to populate the params and used later via get_encryption_params or get_decryption_params
+///        let _ = setup(Some(params_path));
+///
+///        let encryption_params  = get_encryption_params(Some(params_path)).unwrap();
+///        let encrypted_bytes = encrypt(encryption_params, data_to_encrypt_as_bytes).unwrap();
+///
+///        let decryption_params = get_decryption_params(Some(params_path)).unwrap();
+///        let decrypted_bytes = decrypt(decryption_params, encrypted_bytes.as_slice()).unwrap();
+///
+///        assert_eq!(data_to_encrypt_as_bytes, decrypted_bytes);
+///    }
+/// ```
 pub fn encrypt(params: EncryptionParameters, data: &[u8]) -> Result<Vec<u8>, String> {
     let boxed_rsa = Rsa::public_key_from_pem(params.rsa_public_key_pem.as_bytes());
     if boxed_rsa.is_err() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! # crypto-ext
+//!
+//! `crypto-ext` is a set of functionality providing easy and intuitive abstractions to encrypt, decrypt, sign and verify your data.
+//!
+//! ## Features
+//! 1. [Asymmetric cryptography](https://en.wikipedia.org/wiki/Public-key_cryptography) via [RSA](https://en.wikipedia.org/wiki/RSA_(cryptosystem))
+//! 1. [Symmetric cryptography](https://en.wikipedia.org/wiki/Symmetric-key_algorithm) via [AES](https://en.wikipedia.org/wiki/Advanced_Encryption_Standard)
+//! 1. [Digital signature](https://en.wikipedia.org/wiki/Digital_signature) via [ECDSA](https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm)
+//!
+
+
 use std::env;
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
