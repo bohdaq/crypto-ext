351a2c180c4812b7ca778881ff99d4534fca5fba
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-10 01:04:59 +0200

    wip

--- src/symmetric/encryption/mod.rs
+++ src/symmetric/encryption/mod.rs
@@ -1,5 +1,6 @@
 use aes_gcm::aead::{generic_array::GenericArray, Aead, KeyInit, Payload};
 use aes_gcm::Aes128Gcm;
+use crate::{generate_passphrase, get_path_relative_to_working_directory, get_static_filepath, read_or_create_and_write};
 
 #[cfg(test)]
 mod tests;
@@ -14,6 +15,56 @@ pub struct EncryptionParameters {
 pub struct DecryptionParameters {
     pub key: String,
     pub nonce: String,
+}
+
+/// Will read or create EncryptionParameters and DecryptionParameters at the given location which is relative to the working directory
+pub fn setup(path_to_encryption_parameters: Option<&str>) -> Result<(EncryptionParameters, DecryptionParameters), String> {
+    let passphrase_64_bytes = generate_passphrase().unwrap();
+    // key is 16 bytes long
+    let aes_key = passphrase_64_bytes[48..64].to_string();
+    // nonce is 12 bytes long
+    let aes_nonce = passphrase_64_bytes[36..48].to_string();
+
+
+    let relative_path = get_path_relative_to_working_directory(path_to_encryption_parameters, ".aes_key");
+    let boxed_aes_key_path = get_static_filepath(relative_path.as_str());
+    if boxed_aes_key_path.is_err() {
+        return Err(boxed_aes_key_path.err().unwrap());
+    }
+    let aes_key_path = boxed_aes_key_path.unwrap();
+
+    let boxed_aes_key = read_or_create_and_write(aes_key_path.as_str(), aes_key.as_bytes());
+    if boxed_aes_key.is_err() {
+        let message = boxed_aes_key.err().unwrap();
+        return Err(message)
+    }
+
+    let boxed_aes_key = String::from_utf8(boxed_aes_key.unwrap());
+    let aes_key = boxed_aes_key.unwrap();
+
+
+
+
+    let relative_path = get_path_relative_to_working_directory(path_to_encryption_parameters, ".aes_nonce");
+    let boxed_aes_nonce_path = get_static_filepath(relative_path.as_str());
+    if boxed_aes_nonce_path.is_err() {
+        return Err(boxed_aes_nonce_path.err().unwrap());
+    }
+    let aes_nonce_path = boxed_aes_nonce_path.unwrap();
+
+    let boxed_aes_nonce = read_or_create_and_write(aes_nonce_path.as_str(), aes_nonce.as_bytes());
+    if boxed_aes_nonce.is_err() {
+        let message = boxed_aes_nonce.err().unwrap();
+        return Err(message)
+    }
+
+    let boxed_aes_nonce = String::from_utf8(boxed_aes_nonce.unwrap());
+    let aes_nonce = boxed_aes_nonce.unwrap();
+
+    let encryption_params = EncryptionParameters { key: aes_key.to_string(), nonce: aes_nonce.to_string() };
+    let decryption_params = DecryptionParameters { key: aes_key.to_string(), nonce: aes_nonce.to_string() };
+
+    Ok((encryption_params, decryption_params))
 }
 
 pub fn encrypt(params: EncryptionParameters, data_to_encrypt: &[u8], associated_data: &[u8]) -> Result<Vec<u8>, String> {
--- src/symmetric/encryption/tests.rs
+++ src/symmetric/encryption/tests.rs
@@ -1,29 +1,17 @@
-use crate::generate_passphrase;
-use crate::symmetric::encryption::{decrypt, DecryptionParameters, encrypt, EncryptionParameters};
+use crate::symmetric::encryption::{decrypt, encrypt, setup};
 
 #[test]
 fn encryption() {
     let data = "some data to encrypt".as_bytes();
     let associated_data = "some unencrypted data that needs to be sent along the encrypted data and won't be changed during transmission by a hacker".as_bytes();
-
-    let passphrase_64_bytes = generate_passphrase().unwrap();
-
-    // key is 16 bytes long
-    let key = passphrase_64_bytes[48..64].to_string();
-
-    // nonce is 12 bytes long
-    let nonce = passphrase_64_bytes[36..48].to_string();
-
-    let params = EncryptionParameters { key, nonce };
-    let encrypted_data = encrypt(params, data, associated_data).unwrap();
+    let params_path = "/test/encryption_parameters/";
 
 
-    // decryption
-    let key = passphrase_64_bytes[48..64].to_string();
-    let nonce = passphrase_64_bytes[36..48].to_string();
+    let (encryption_params, decryption_params) = setup(Some(params_path)).unwrap();
 
-    let params = DecryptionParameters{ key, nonce };
-    let decrypted = decrypt(params, encrypted_data.as_slice(), associated_data).unwrap();
+    let encrypted_data = encrypt(encryption_params, data, associated_data).unwrap();
+
+    let decrypted = decrypt(decryption_params, encrypted_data.as_slice(), associated_data).unwrap();
 
     assert_eq!(data, decrypted);
 
