6fc03013ae9ab2b5c315d966787882f283983e9b
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-09 02:01:22 +0200

    wip

--- src/asymmetric/signing/mod.rs
+++ src/asymmetric/signing/mod.rs
@@ -57,6 +57,30 @@ pub fn get_verification_params(path_to_encryption_parameters: Option<&str>) -> R
     Ok(params)
 }
 
+/// Signs given byte array
+///
+/// # Examples
+///
+/// ```
+///     use crypto_ext::asymmetric::signing::{get_signature_params, get_verification_params, setup, sign, verify};
+///     #[test]
+///     fn signing() {
+///         let data = "some data to sign".as_bytes();
+///         let path_to_params = "/test/signature_parameters/";
+///
+///         // setup will read, or populate required parameters for signing and verification
+///         // in this example setup is used to populate the required parameters
+///         let _ = setup(Some(path_to_params)).unwrap();
+///
+///         let signature_params = get_signature_params(Some(path_to_params)).unwrap();
+///         let signature = sign(signature_params, data).unwrap();
+///
+///         let verification_params = get_verification_params(Some(path_to_params)).unwrap();
+///         let verified = verify(verification_params, data, signature.as_slice());
+///
+///         assert!(verified.is_ok());
+///     }
+/// ```
 pub fn sign(params: SignatureParameters, data: &[u8]) -> Result<Vec<u8>, String> {
     let signing_key = SigningKey::from_bytes(params.ecdsa_private_key.as_slice()).unwrap();
     let signature = signing_key.sign(data);
