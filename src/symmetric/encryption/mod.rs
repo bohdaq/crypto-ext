5b9572bfe6782c3232b659554b1d270faef91137
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-10 04:22:39 +0200

    wip

--- src/symmetric/encryption/mod.rs
+++ src/symmetric/encryption/mod.rs
@@ -126,6 +126,35 @@ pub fn get_decryption_params(path_to_encryption_parameters: Option<&str>) -> Res
     Ok(decryption_params)
 }
 
+/// Encrypts given byte array
+///
+/// # Examples
+///
+/// ```
+///     use crypto_ext::symmetric::encryption::{decrypt, encrypt, get_decryption_params, get_encryption_params, setup};
+///
+///     #[test]
+///     fn encryption() {
+///         let data = "some data to encrypt".as_bytes();
+///         let associated_data = "some unencrypted data that needs to be sent along the encrypted data and won't be changed during transmission by a hacker".as_bytes();
+///
+///         // path needs to be accessible by user with write permission for initial setup
+///         // ideally for each encryption you need to setup unique folder for AES key and nonce
+///         // do not reuse same setup with Encryption and Decryption parameters for multiple encryptions
+///         let params_path = "/test/encryption_parameters/";
+///
+///
+///         let (encryption_params, decryption_params) = setup(Some(params_path)).unwrap();
+///
+///         let encrypted = encrypt(encryption_params, data, associated_data).unwrap();
+///
+///         let decrypted = decrypt(decryption_params, encrypted.as_slice(), associated_data).unwrap();
+///
+///         assert_eq!(data, decrypted);
+///
+///     }
+///
+/// ```
 pub fn encrypt(params: EncryptionParameters, data_to_encrypt: &[u8], associated_data: &[u8]) -> Result<Vec<u8>, String> {
     let payload = Payload {
         msg: data_to_encrypt,
