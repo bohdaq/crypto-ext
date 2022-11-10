1f787b3d51104d3e74e8ecef4aa0cee51d79679f
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-10 04:01:30 +0200

    wip

--- src/symmetric/encryption/tests.rs
+++ src/symmetric/encryption/tests.rs
@@ -5,6 +5,7 @@ fn encryption() {
     let data = "some data to encrypt".as_bytes();
     let associated_data = "some unencrypted data that needs to be sent along the encrypted data and won't be changed during transmission by a hacker".as_bytes();
 
+    // path needs to be accessible by user with write permission for initial setup
     // ideally for each encryption you need to setup unique folder for AES key and nonce
     // do not reuse same setup with Encryption and Decryption parameters for multiple encryptions
     let params_path = "/test/encryption_parameters/";
@@ -22,9 +23,11 @@ fn encryption() {
 
 #[test]
 fn decryption() {
+    // to decrypt we need to encrypt first
     let data = "some data to encrypt".as_bytes();
     let associated_data = "some unencrypted data that needs to be sent along the encrypted data and won't be changed during transmission by a hacker".as_bytes();
 
+    // path needs to be accessible by user with write permission for initial setup
     // ideally for each encryption you need to setup unique folder for AES key and nonce
     // do not reuse same setup with Encryption and Decryption parameters for multiple encryptions
     let params_path = "/test/encryption_parameters/";
